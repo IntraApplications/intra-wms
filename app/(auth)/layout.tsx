07a152c566821c07077793d38edf714828581d54
KDL <kdlkly01@gmail.com>
2024-09-10 09:36:12 -0400

    chore(auth): reverted to client side auth temporarily for testing

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,26 +1,149 @@
-import React from "react";
-import { redirect } from "next/navigation";
-import { createClient } from "@/_lib/supabase/supabase-server";
+"use client";
+
+import React, { useState } from "react";
+import axios from "axios";
+import { useMutation } from "@tanstack/react-query";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+
 import { faGithub, faGoogle } from "@fortawesome/free-brands-svg-icons";
-import OauthButton from "@/_common/components/OauthButton";
 import Input from "@/_common/components/Input";
 import Button from "@/_common/components/Button";
-import { handleLogin } from "./actions";
+import OauthButton from "@/_common/components/OauthButton";
+import { supabase } from "@/_lib/supabase";
+import { usePathname } from "next/navigation";
+import { useWebSocketContext } from "@/contexts/WebSocketContext";
 
-export default async function AuthLayout({
+type LoginInputs = {
+  username: string;
+  password: string;
+};
+
+const loginSchema = z.object({
+  username: z.string().email("Please enter a valid email address."),
+  password: z
+    .string()
+    .min(7, "Password must be at least 7 characters long.")
+    .max(25, "Password cannot exceed 25 characters."),
+});
+
+export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const supabase = createClient();
+  const pathname = usePathname();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { showNotification } = useWebSocketContext();
+
+  const isLoginPage = pathname === "/login";
+  const isSignupPage = pathname === "/signup";
 
   const {
-    data: { session },
-  } = await supabase.auth.getSession();
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginInputs>({
+    resolver: zodResolver(loginSchema),
+  });
 
-  if (session) {
-    redirect("/dashboard/workspaces");
-  }
+  const loginMutation = useMutation({
+    mutationFn: (credentials: LoginInputs) => {
+      return axios.post("/api/login", credentials);
+    },
+    onError: (error) => {
+      showNotification({
+        type: "error",
+        title: "Login Error",
+        message: "Login failed. Please check your credentials and try again.",
+      });
+    },
+  });
+
+  const onSubmit: SubmitHandler<LoginInputs> = async (data: LoginInputs) => {
+    setIsLoading(true);
+    if (isLoginPage) {
+      try {
+        const signupData = await supabase.auth.signInWithPassword({
+          email: data.username,
+          password: data.password,
+        });
+
+        if (signupData.error) {
+          throw new Error(signupData.error.message);
+        }
+      } catch (err) {
+        showNotification({
+          type: "error",
+          title: "Login Error",
+          message: err.message || "Log in failed. Please try again.",
+        });
+      } finally {
+        setIsLoading(false);
+      }
+    } else if (isSignupPage) {
+      try {
+        const signupData = await supabase.auth.signUp({
+          email: data.username,
+          password: data.password,
+          options: {
+            emailRedirectTo: "http://localhost:3000/dashboard",
+          },
+        });
+
+        if (signupData.error) {
+          throw new Error(signupData.error.message);
+        }
+      } catch (err) {
+        showNotification({
+          type: "error",
+          title: "Signup Error",
+          message: err.message || "Sign up failed. Please try again.",
+        });
+      } finally {
+        setIsLoading(false);
+      }
+    } else {
+      setIsLoading(false);
+      showNotification({
+        type: "error",
+        title: "Navigation Error",
+        message: "Invalid login page",
+      });
+    }
+  };
+
+  const onOauthSubmit = async (provider: "google" | "github") => {
+    try {
+      let error;
+      if (provider === "google") {
+        ({ error } = await supabase.auth.signInWithOAuth({
+          provider: "google",
+          options: {
+            redirectTo: `http://localhost:3000/dashboard/workspaces`,
+          },
+        }));
+      } else {
+        ({ error } = await supabase.auth.signInWithOAuth({
+          provider: "github",
+          options: {
+            redirectTo: `http://localhost:3000/dashboard/workspaces`,
+          },
+        }));
+      }
+
+      if (error) {
+        throw new Error(error.message);
+      }
+    } catch (err) {
+      showNotification({
+        type: "error",
+        title: "OAuth Error",
+        message: err.message || "OAuth login failed. Please try again.",
+      });
+    }
+  };
 
   return (
     <div className="flex h-screen items-center justify-center bg-red flex-1">
@@ -29,48 +152,52 @@ export default async function AuthLayout({
 
         <div className="mt-6 w-11/12 sm:w-full">
           <div className="justify-center">
-            <div className="grid gap-4">
-              <OauthButton
-                icon={faGoogle}
-                text="Continue with Google"
-                oauthType="google"
-              />
-
-              <OauthButton
-                icon={faGithub}
-                text="Continue with Github"
-                oauthType="github"
-              />
-              <div className="flex items-center mt-2 justify-center">
-                <div className="flex-grow border-t border-secondary"></div>
-                <span className="px-4 text-sm text-accent">or</span>
-                <div className="flex-grow border-t border-secondary"></div>
+            <form onSubmit={handleSubmit(onSubmit)} className="justify-center">
+              <div className="grid gap-4">
+                <OauthButton
+                  icon={faGoogle}
+                  text={"Continue with Google"}
+                  type={"button"}
+                  oauthType="google"
+                  handleClick={() => onOauthSubmit("google")}
+                />
+                <OauthButton
+                  icon={faGithub}
+                  text={"Continue with Github"}
+                  type={"button"}
+                  oauthType="github"
+                  handleClick={() => onOauthSubmit("github")}
+                />
+                <div className="flex items-center mt-2 justify-center">
+                  <div className="flex-grow border-t border-secondary"></div>
+                  <span className="px-4 text-sm text-accent">or</span>
+                  <div className="flex-grow border-t border-secondary"></div>
+                </div>
               </div>
-            </div>
 
-            <form action={handleLogin}>
               <div className="grid mb-8">
                 <Input
                   label="Email Address"
                   id="email"
-                  name="email"
                   autoComplete="email"
+                  error={errors.username}
                   placeholder="Enter your email"
+                  {...register("username", { required: true })}
                 />
                 <div className="relative">
                   <Input
                     label="Password"
                     id="password"
-                    name="password"
-                    type="password"
                     autoComplete="current-password"
+                    error={errors.password}
+                    {...register("password", { required: true })}
                     placeholder="Enter your password"
                   />
                   <div className="grid mt-6 absolute right-0 top-0">
                     <div className="text-xs text-center mb-2">
                       <a
                         href="#"
-                        className="font-normal text-secondaryBorder hover:brightness-125"
+                        className=" font-normal text-secondaryBorder hover:brightness-125"
                       >
                         Forgot password?
                       </a>
@@ -79,7 +206,12 @@ export default async function AuthLayout({
                 </div>
               </div>
 
-              <Button text={"Log in"} type={"submit"} colorType="tertiary" />
+              <Button
+                text={"Log in"}
+                type={"submit"}
+                handleClick={() => {}}
+                loading={isLoading}
+              />
             </form>
           </div>
         </div>
---
+++ app/(auth)/layout2.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { redirect } from "next/navigation";
+import { createClient } from "@/_lib/supabase/supabase-server";
+import { faGithub, faGoogle } from "@fortawesome/free-brands-svg-icons";
+import OauthButton from "@/_common/components/OauthButton";
+import Input from "@/_common/components/Input";
+import Button from "@/_common/components/Button";
+import { handleLogin } from "./actions";
+
+export default async function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const supabase = createClient();
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (session) {
+    redirect("/dashboard/workspaces");
+  }
+
+  return (
+    <div className="flex h-screen items-center justify-center bg-red flex-1">
+      <div className="flex flex-col w-full max-w-sm lg:w-96 items-center justify-center">
+        {children}
+
+        <div className="mt-6 w-11/12 sm:w-full">
+          <div className="justify-center">
+            <div className="grid gap-4">
+              <OauthButton
+                icon={faGoogle}
+                text="Continue with Google"
+                oauthType="google"
+              />
+
+              <OauthButton
+                icon={faGithub}
+                text="Continue with Github"
+                oauthType="github"
+              />
+              <div className="flex items-center mt-2 justify-center">
+                <div className="flex-grow border-t border-secondary"></div>
+                <span className="px-4 text-sm text-accent">or</span>
+                <div className="flex-grow border-t border-secondary"></div>
+              </div>
+            </div>
+
+            <form action={handleLogin}>
+              <div className="grid mb-8">
+                <Input
+                  label="Email Address"
+                  id="email"
+                  name="email"
+                  autoComplete="email"
+                  placeholder="Enter your email"
+                />
+                <div className="relative">
+                  <Input
+                    label="Password"
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    placeholder="Enter your password"
+                  />
+                  <div className="grid mt-6 absolute right-0 top-0">
+                    <div className="text-xs text-center mb-2">
+                      <a
+                        href="#"
+                        className="font-normal text-secondaryBorder hover:brightness-125"
+                      >
+                        Forgot password?
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <Button text={"Log in"} type={"submit"} colorType="tertiary" />
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
